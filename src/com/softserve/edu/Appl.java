02f6c64d7b48c29c35718f212f2267323500f7a1
yharasym <garasym@gmail.com>
2020-10-19 17:49:17 +0300

    Add private field

--- src/com/softserve/edu/Appl.java
+++ src/com/softserve/edu/Appl.java
@@ -1,7 +1,20 @@
 package com.softserve.edu;
 
 public class Appl {
+    private int i = 123;
+    
+    // JVM add default Constructor
+    //public Appl() {
+    //} 
+
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        Appl app1 = new Appl();
+        Appl app2 = new Appl();
+        //
+        app1.i = 234;
+        app2.i = 4321;
+        System.out.println("app1.i = " + app1.i);
+        System.out.println("app2.i = " + app2.i);
     }
 }
