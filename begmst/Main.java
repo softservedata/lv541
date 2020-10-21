ae4789e522067d5a6f3bf898b80d2b9dcd1029fc
Mstislav Begunov <mstislav.begunov@together.com>
2020-10-21 18:06:15 +0300

    Demo input/output

---
+++ begmst/Main.java
@@ -0,0 +1,18 @@
+package begmst;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.print("Enter your name: ");
+		Scanner s = new Scanner(System.in);
+		String name = s.nextLine();
+		System.out.print("Enter your age: ");
+		Integer age = s.nextInt();
+		String output = String.format("Hello, %s. You're %d years old.", name, age);
+		System.out.println(output);
+	}
+
+}
