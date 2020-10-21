119ec37ae32b579f8bde6d48b5c58b6dae2d0bb0
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2020-10-21 09:50:39 +0300

    My first Main Method

---
+++ src/com/softserve/edu/Main.java
@@ -0,0 +1,11 @@
+package com.softserve.edu;
+
+public class Main {
+	static int myMethod(int x, int y) {
+			return x + y;
+		}
+		public static void main(String[] args) {
+        System.out.println(myMethod(2356, 856));
+    }
+}
+// Outputs 3212 
