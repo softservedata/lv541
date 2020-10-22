dff68702f5991977888e020934486c5d44f5d540
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2020-10-22 22:29:34 +0300

    Task1

--- src/com/softserve/edu/FlowerBed.java
+++ src/com/softserve/edu/FlowerBed.java
@@ -1,5 +1,16 @@
 package com.softserve.edu;
-
-public class FlowerBed {
+import java.util.Scanner;
+class FlowerBed {
+static Scanner sc = new Scanner (System.in);
+public static void main(String [] args) {
+	System.out.print("Enter the radius: ");
+	double radius = sc.nextDouble();
+	double area =Math.PI * (radius * radius);
+	System.out.println("The area of the circle is: " + area);
+	double circumference =Math.PI * 2*radius;
+		System.out.println("The circumference of the circle is: " + circumference);
+	
+	
+}
 
 }
