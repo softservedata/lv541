b6c6c37e1a1fc5f0f3e77ac35b160ac18f63f753
ZhannaPy <zhannapysanka@gmail.com>
2020-10-25 16:15:58 +0200

    add tesk1_1

--- src/com/softserve/task1_1/Task1.java
+++ src/com/softserve/task1_1/Task1.java
@@ -1,9 +1,27 @@
 package com.softserve.task1_1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Task1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		int a,b;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//enter a
+		System.out.print("enter a=");
+		a = Integer.parseInt(br.readLine());
+		//enter b
+		System.out.print("enter b=");
+		b = Integer.parseInt(br.readLine());
+		br.close();
+		//calculate:
+		System.out.println("a+b="+(a+b));
+		System.out.println("a-b="+(a-b));
+		System.out.println("a*b="+(a*b));
+		System.out.println("a/b="+(a/b));
+		
 
 	}
 
