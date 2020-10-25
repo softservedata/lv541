39dd7c4a697ae265b897aafe40a78cb2e2026e5e
ZhannaPy <zhannapysanka@gmail.com>
2020-10-25 16:55:28 +0200

    home work 2

---
+++ src/com/softserve/HomeWork1_2/HW2.java
@@ -0,0 +1,21 @@
+package com.softserve.HomeWork1_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HW2 {
+
+	public static void main(String[] args) throws IOException {
+		String name,address;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("What is your name?");
+		name = br.readLine();
+		System.out.println("Where are you live, "+name);
+		address = br.readLine();
+		br.close();
+		System.out.println("Your name is "+name+" and your adress is "+address);
+
+	}
+
+}
