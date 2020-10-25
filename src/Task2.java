c5ece571090d38384d66a8b08c63507fd8d1a7af
khrystyna <khristina7388@gmail.com>
2020-10-25 21:23:23 +0200

    Homeworkk

--- src/Task2.java
+++ src/Task2.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        String address = scanner.nextLine();
         System.out.println("What is your name?");
+        String name = scanner.nextLine();
         System.out.println("Where do you live, " + name + " ?");
+        String address = scanner.nextLine();
         System.out.println("I live on " + address + " street.");
     }
 }
