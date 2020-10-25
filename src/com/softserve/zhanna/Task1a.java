6cdd61ad5dc4e5485445a9fdb4a3a46bf5523414
ZhannaPy <zhannapysanka@gmail.com>
2020-10-25 14:52:21 +0200

    test

--- src/com/softserve/zhanna/Task1a.java
+++ src/com/softserve/zhanna/Task1a.java
@@ -1,8 +1,13 @@
 package com.softserve.zhanna;
 
 public class Task1a {
+	private int k = 123;
 	public static void main(String[] args) {
+		
 		System.out.println("HI");
+		Task1a Ap = new Task1a();
+		Ap.k = 123;
+		System.out.println("Ap.k  = "+ Ap.k );
 	}
 
 }
