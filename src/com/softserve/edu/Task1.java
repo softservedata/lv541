2eed2fd2ec0e330d85ae20cbb813a70b1014c2b8
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2020-11-03 15:13:55 +0200

    Homework4_Task_3

--- src/com/softserve/edu/Task1.java
+++ src/com/softserve/edu/Task1.java
@@ -1,22 +1,33 @@
 package com.softserve.edu;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task1 {
-	
 	public static void main (String [] args) {
+		
 		Scanner scanner =new Scanner(System.in);
-	int numbersPosition [] = new int [5];
-int sum =0;
-System.out.println("Eneter number : ");
-for ( int i=0; i<5; i++)
-{
-numbersPosition[i]= scanner.nextInt();
-}
-for ( int num : numbersPosition) {
-	sum = sum+sum;
-}
-	System.out.println("Sum of array ellemnts is : " + sum);
+		System.out.println("number elements");
+		int n =scanner.nextInt();
+		int arr[] = new int [n];
+	      System.out.println("Enter the elements of the array one by one ");
+
+	      for(int i=0; i<n; i++)
+	      {
+	         arr[i] = scanner.nextInt();
+	      }
+	      for (int i:arr) {
+	      System.out.println(i);
+	   }
+	int sum =0;
+	for (int i=0; i< arr.length; i++) {sum +=arr[i];}
+	System.out.println("Sum = " + sum);
+	int product = 1;
+	for (int i=0;i<5; i++) {
+		if (arr[i]>0) {product = product * arr[i];}}
+	System.out.println("Product = " + product);
 	}
-	
-}
+	}
+	     
+
+
