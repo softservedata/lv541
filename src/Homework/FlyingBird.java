421e2d09d86e038dbd16c8a544cd433d85a807c1
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2020-11-09 13:16:39 +0200

    FlyingBird

--- src/Homework/FlyingBird.java
+++ src/Homework/FlyingBird.java
@@ -1,5 +1,14 @@
 package Homework;
 
 public class FlyingBird extends Bird {
-
+ public FlyingBird(boolean feathers, int layEggs) {
+	 super(feathers, layEggs);
+ }
+	
+	
+	@Override
+ boolean fly() {
+ return true;
+ }
+ 
 }
