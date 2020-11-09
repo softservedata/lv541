d4cdb9858491a67ee985cd46209259a30c09ef96
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2020-11-09 13:49:27 +0200

    Penguin

--- src/Homework/Penguin.java
+++ src/Homework/Penguin.java
@@ -1,5 +1,27 @@
 package Homework;
 
-public class Penguin {
+public class Penguin extends NonFlyingBird {
+	private String name;
+
+	public Penguin(boolean feathers, int layEggs, String name) {
+		super(feathers, layEggs);
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Penguin{" + "name=' " + name + "}" +  super.toString();
+	}
+	
+	
+	
 
 }
