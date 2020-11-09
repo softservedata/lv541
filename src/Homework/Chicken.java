515e22ecac1bab26e4cd529c4e56292d42b53cc0
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2020-11-09 13:49:49 +0200

    Chicken

--- src/Homework/Chicken.java
+++ src/Homework/Chicken.java
@@ -1,5 +1,27 @@
 package Homework;
 
-public class Chicken {
+public class Chicken extends NonFlyingBird {
+	private String name;
+
+	public Chicken(boolean feathers, int layEggs, String name) {
+		super(feathers, layEggs);
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Chicken{" +
+	"name=" + name + "}" + super.toString();
+	} 
+	
+	
 
 }
