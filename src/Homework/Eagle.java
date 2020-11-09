8b736c5fddbe46476c3107997b5fe71d25b10f2d
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2020-11-09 13:48:57 +0200

    Eagle

--- src/Homework/Eagle.java
+++ src/Homework/Eagle.java
@@ -1,5 +1,25 @@
 package Homework;
 
-public class Eagle {
+public class Eagle extends FlyingBird {
+	private String name;
+
+	public Eagle(boolean feathers, int layEggs, String name) {
+		super(feathers, layEggs);
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Eagle{" + "name=' " + name + "}" +  super.toString();
+	}
+	
 
 }
