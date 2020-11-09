be62762bfc19c4c517e711d30a7273c3c8ec8c3d
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2020-11-09 13:15:40 +0200

    Bird

--- src/Homework/Bird.java
+++ src/Homework/Bird.java
@@ -1,5 +1,43 @@
 package Homework;
 
-public class Bird {
+public abstract class Bird {
+	abstract boolean fly();
+	private boolean feather;
+	private int layEggs;
+	
+	public Bird() {
+		layEggs=0;
+		feather =true;
+	}
 
-}
+	public Bird(boolean feather, int layEggs) {
+		
+		this.feather = feather;
+		this.layEggs = layEggs;
+	}
+
+	public boolean isFeather() {
+		return feather;
+	}
+
+	public void setFeather(boolean feather) {
+		this.feather = feather;
+	}
+
+	public int getLayEggs() {
+		return layEggs;
+	}
+
+	public void setLayEggs(int layEggs) {
+		this.layEggs = layEggs;
+	}
+
+	@Override
+	public String toString() {
+		return "Bird [feather=" + feather + ", layEggs=" + layEggs + "]";
+	}
+
+
+	}
+	
+
