ba6e0a6a0cf1ab4303f971e076250d1a779c0b95
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2020-11-13 09:32:43 +0200

    Employee Template

--- src/Homework_Task2/Employee.java
+++ src/Homework_Task2/Employee.java
@@ -1,35 +1,53 @@
 package Homework_Task2;
 
-public class Employee {
+public  class Employee  implements PayMethod, Comparable<Employee>{
     public  String employeeId;
     public String Name;
-	
+    
+   
+ // Constructor 	
     public Employee(String employeeId, String name) {
-		super();
-		this.employeeId = employeeId;
-		Name = name;
+		employeeId = "";
+		Name = "";
 	}
+
 
 	public String getEmployeeId() {
 		return employeeId;
 	}
 
+
 	public void setEmployeeId(String employeeId) {
 		this.employeeId = employeeId;
 	}
+
 
 	public String getName() {
 		return Name;
 	}
 
+
 	public void setName(String name) {
 		Name = name;
 	}
+
 
 	@Override
 	public String toString() {
 		return "Employee [employeeId=" + employeeId + ", Name=" + Name + "]";
 	}
-    
-  
+
+
+	@Override
+	public int compareTo(Employee employee) {
+		
+		return (int) (this.calculatePay() - employee.calculatePay());
+	}
+
+
+	@Override
+	public double calculatePay() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
