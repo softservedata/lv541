8d9390f4e5209b657227a7e1e69f37f4ec6a0551
bebebe <hahaha@gmail.com>
2020-11-16 16:35:13 +0200

    Java Regex. Examples

--- src/com/softserve/edu09/ApplRex.java
+++ src/com/softserve/edu09/ApplRex.java
@@ -1,5 +1,7 @@
 package com.softserve.edu09;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,11 +13,23 @@ public class ApplRex {
         //String text = "Now is the time";
         //
         //
-        String pattern = "<.+>";
-        // String pattern = "<[^>]+>"; // All tags
+        //String pattern = "<.+>";
+        //String pattern = "<[^>]+>"; // All tags
         //String pattern = ">[^><]+<"; // All text
         //String pattern = "<(.+)>[^<]+</\\1>";
-        String text = "<p><b>Beginning with bold text</b> next, <span>text</span> body,<i>italic text</i> end of text.</p>";
+        //String text = "<p><b>Beginning with bold text</b> next, <span>text</span> body,<i>italic text</i> end of text.</p>";
+        //
+        //String pattern = "\\w+(\\.\\w+)*@(\\w+\\.)+\\w{2,}";
+        //String text = "a.b.c@gmail.com";
+        //String text = "a.b.c@gmail.com a@i.ua  a.@gmail.com a@gmail.c";
+        //
+        //String pattern = "\\b(\\d{1,3}[,'])*\\d{1,3}\\.\\d{2}\\b";
+        //String pattern = "\\b\\d{1,3}([,']\\d{3})*\\.\\d{2}\\b";
+        //String text = "4 item(s) - $1'121,450.40";
+        //
+        //String pattern ="([\\w])\\1";
+        String pattern ="\\b\\w*([\\w])\\1\\w*\\b";
+        String text = "letter abba work";
         //
         Pattern p = Pattern.compile(pattern);
         Matcher m = p.matcher(text);
@@ -27,12 +41,13 @@ public class ApplRex {
         //
         // 2. Get all parts
         m.reset();
-        // List<String> list = new ArrayList<>();
+        //List<String> list = new ArrayList<>();
         while (m.find()) {
             System.out.print(text.substring(m.start(), m.end()) + "*");
-            // System.out.print(text.substring(m.start() + 1, m.end() - 1).trim() + "*");
-            // list.add(text.substring(m.start(), m.end()));
+            //System.out.print(text.substring(m.start() + 1, m.end() - 1).trim() + "*");
+            //list.add(text.substring(m.start(), m.end()));
         }
+//        System.out.println();
 //        for (String current : list) {
 //            System.out.println(current);
 //        }
