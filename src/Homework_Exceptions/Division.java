1a55b3b5eecfb726d7d3f7f8e9f23115e6996520
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2020-12-01 14:50:09 +0200

    Division

--- src/Homework_Exceptions/Division.java
+++ src/Homework_Exceptions/Division.java
@@ -1,32 +1,42 @@
 package Homework_Exceptions;
 
 import java.math.*;
+import java.util.Scanner;
 
 
 public class Division {
 
 	public static void main(String[] args) {
-		Division object = new Division();
-		double result = 0;
-		
-		try {
-			result = object.div (50.5,0);
-		} catch (NumberFormatException numformexp) {
-			System.out.println ("Number Format is wrong");
-		}catch (Exception e){
-		 e.printStackTrace();
-		System.out.println ("Exception");
-		}
-		System.out.println(result);
-		}
-
-	public double div ( double a, double b) throws ArithmeticException {
-		if (b ==0) {
-			throw new ArithmeticException ("Arithmetic Exception ");
-		}
-		return a/b;
-		
+		Division.Task1();
 	}
+		public static double div (double a, double b) {
+		return a /b; 	
+	}
+		public static void Task1() {
+			
+			Scanner sc = new Scanner (System.in);
+			double a;
+			double b; 
+			while(true) {
+				try {
+					System.out.print("Enter number");
+					a = Double.parseDouble(sc.nextLine());
+					
+					System.out.print("Enter number");
+					b = Double.parseDouble(sc.nextLine());
+					
+					double res = Division.div(a, b);
+					System.out.println (res);
+					break;
+				}catch (ArithmeticException e) {
+					System.out.println ("Arithmetic exception");
+				}catch (NumberFormatException e) {
+					System.out.println("NumberFormatException ");
+				} catch (IndexOutOfBoundsException e) {
+					System.out.println("IndexOutOfBoundsException ");
+			}
+		}
+}
 }
 
 
